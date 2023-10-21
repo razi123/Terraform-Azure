b3659f2fe43637decb51acccb54b05bc2fc67f86
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-21 09:42:43 +0200

    resource and location hardcoded

--- terra_config/main.tf
+++ terra_config/main.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group"{
-    name = var.my_resource_group_name
-    location = var.my_resource_group_location
+    name = "rg_benz"   #"var.my_resource_group_name"
+    location = "westeurope" # var.my_resource_group_location
 }
