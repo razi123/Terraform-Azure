1bde4fc9599616317aa5a44823ea55ea7c864e71
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-21 09:24:09 +0200

    test_automate

--- terra_config/main.tf
+++ terra_config/main.tf
@@ -10,14 +10,10 @@ terraform {
 # Authentication to Azure 
 provider "azurerm" {
   features {}
-  subscription_id = "7ca5fc28-3bbc-47b0-960d-4dba56edc701"
-  client_id = "45bcf450-c37e-448e-976c-cecda4b9ddbf"
-  client_secret = "DAK8Q~L-Lx55bfI7GzYbpEav-3J-fV46wFCLwavW"
-  tenant_id = "208effc2-b43b-40b9-a5b1-8882b8d51171"
   skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "resource_group"{
-    name = "rg_benz"
-    location = "West Europe"
+    name = var.my_resource_group_name
+    location = var.my_resource_group_location
 }
---
+++ terra_config/variables.tf
@@ -0,0 +1,9 @@
+variable "my_resource_group_name" {
+    type = string
+    description = "my_resource_group"
+}
+
+variable "my_resource_group_location" {
+    type = string
+    description = "location of my RG"
+}
