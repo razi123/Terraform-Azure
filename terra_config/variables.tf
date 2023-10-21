6876e46b00b55888d2dfad2145958792b3d62b8b
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-21 09:46:09 +0200

    remove variable, typo on variable changed

--- terra_config/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "my_resource_group_name" {
-    type = string
-    description = "my_resource_group"
-}
-
-variable "my_resource_group_location" {
-    type = string
-    description = "location of my RG"
-}
