dd8ce87b0733dc1d1a7ee61c849c3a8b110de5f2
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-27 21:08:56 +0200

    test SP and RG

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
-module "databricks" {
-  source                   = "./modules/databricks"
-  resource_group_name = var.resource_group_name
-  location                 = var.location
-  client_id                = var.client_id
-  client_secret            = var.client_secret
-  tenant_id                = var.tenant_id
-  databricks_cluster_name  = var.databricks_cluster_name
-  spark_version            = var.spark_version
-  driver_node_type_id      = var.driver_node_type_id
-  node_type_id             = var.node_type_id
+# module "databricks" {
+#   source                   = "./modules/databricks"
+#   resource_group_name = var.resource_group_name
+#   location                 = var.location
+#   client_id                = var.client_id
+#   client_secret            = var.client_secret
+#   tenant_id                = var.tenant_id
+#   databricks_cluster_name  = var.databricks_cluster_name
+#   spark_version            = var.spark_version
+#   driver_node_type_id      = var.driver_node_type_id
+#   node_type_id             = var.node_type_id
+# }
+
+terraform {
+  required_providers {  
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.76.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+# add Resource Group
+resource "azurerm_resource_group" "resource_group"{
+    name = var.resource_group_name.default
+    location = var.location.default
+}
