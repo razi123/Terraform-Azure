92ad693d7df0269cc779f00e2e921fd2dd975a2a
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-28 18:23:12 +0200

    hardcode RG and loca

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@
 
 # add Resource Group
 resource "azurerm_resource_group" "resource_group"{
-    name = var.resource_group_name.default
-    location = var.location.default
+    name = "benz_rg"    #var.resource_group_name.default
+    location = "westeurope" # var.location.default
 }
