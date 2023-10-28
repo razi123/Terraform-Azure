be28ac86c937e47dec1efc867a0ddeab57a9ec3d
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-28 18:14:20 +0200

    terrafrom rg deploy

--- providers.tf
+++ providers.tf
@@ -18,21 +18,9 @@ terraform {
 # Authentication to Azure
 provider "azurerm" {
     features {}
-    subscription_id     = "7ca5fc28-3bbc-47b0-960d-4dba56edc701"
-    tenant_id           = "208effc2-b43b-40b9-a5b1-8882b8d51171"
     skip_provider_registration = true
 }
 
 provider "databricks" {
 
-}
-
-data "azurerm_key_vault_secrets" "kv-check" {
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
-
-data "azurerm_key_vault_secret" "kv-secrets" {
-  for_each     = toset(data.azurerm_key_vault_secrets.kv-check.names)
-  name         = each.key
-  key_vault_id = data.azurerm_key_vault.existing.id
 }
