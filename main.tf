f19e0da875abcfbbdc6edcd88d77fcf20d47add1
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-28 19:07:13 +0200

    default added

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@
 
 # add Resource Group
 resource "azurerm_resource_group" "resource_group"{
-    name = var.resource_group_name.name
-    location = var.location.name
+    name = var.resource_group_name.default
+    location = var.location.default
 }
