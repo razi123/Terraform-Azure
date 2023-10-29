bb6c08753fd414074a2b803873f02d11b8a4f4ae
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-29 21:04:32 +0100

    try3

--- providers.tf
+++ providers.tf
@@ -25,7 +25,7 @@ provider "databricks" {
   #host       = "https://accounts.azuredatabricks.net"
   host = azurerm_databricks_workspace.DB_workspace.workspace_url
   azure_workspace_resource_id = azurerm_databricks_workspace.DB_workspace.id
-  azure_client_id             = var.client_id
-  azure_client_secret         = var.client_secret
-  azure_tenant_id             = var.tenant_id
+  # azure_client_id             = var.client_id
+  # azure_client_secret         = var.client_secret
+  # azure_tenant_id             = var.tenant_id
 }
