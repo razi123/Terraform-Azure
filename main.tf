2fed465b1704e13b21a2ed652cac3eb1bc2ecfd3
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-31 09:35:36 +0100

    only dbricks

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "azure_storage" {
-  source                       = "./modules/storage"
-  resource_group_name_commons  = var.resource_group_name_commons
-  storage_account_name_commons = var.storage_account_name_commons
-  location                     = var.location
-  vnet-emea-commons            = var.vnet-emea-commons
-  subnet-emea-commons          = var.subnet-emea-commons
+# module "azure_storage" {
+#   source                       = "./modules/storage"
+#   resource_group_name_commons  = var.resource_group_name_commons
+#   storage_account_name_commons = var.storage_account_name_commons
+#   location                     = var.location
+#   vnet-emea-commons            = var.vnet-emea-commons
+#   subnet-emea-commons          = var.subnet-emea-commons
 
-}
+# }
 
 module "databricks" {
   source                  = "./modules/databricks"
