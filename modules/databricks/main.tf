8d43360b7347d65b7cda79ef35b97e21b0379dd1
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-31 16:00:44 +0100

    PR request created

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 }
 
-# add second resoruce group
+# add resource group
 resource "azurerm_resource_group" "resource_group_name" {
   name = var.resource_group_name
   location = var.location
