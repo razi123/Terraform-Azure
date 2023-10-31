0d7cb5cd84ee3d4286cfc1621046e4898f0cc37a
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-31 11:56:24 +0100

    try something new

--- variables.tf
+++ variables.tf
@@ -7,18 +7,3 @@ variable "location" {
   type        = string
   default     = "westeurope"
 }
-
-# variable "client_id" {
-#   default = "81039d54-e89a-4794-a379-e0229c6c0982"
-# }
-
-# variable "client_secret" {
-#   default = "2Ik8Q~8~sRPZfBp406iSLalz9REuFwYtgMLMgb7c"
-# }
-
-# variable "subscription_id" {
-#   default = "7ca5fc28-3bbc-47b0-960d-4dba56edc701"
-# }
-
-# variable "tenant_id" {
-#   default = "208effc2-b43b-40b9-a5b1-8882b8d51171"
